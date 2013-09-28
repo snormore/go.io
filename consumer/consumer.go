2ec62fb82ecba3e3454cee24e4a3b24b2a234f01
Steven Normore <snormore@gmail.com>
2013-09-28 02:34:16 -0400

    Fixes consumer messages channel naming.

--- consumer/consumer.go
+++ consumer/consumer.go
@@ -15,10 +15,10 @@ func NewConsumer() Consumer {
 	return Consumer{}
 }
 
-func (self *Consumer) Listen(messageChannel chan dispatcher_message.Message, t *tomb.Tomb) {
+func (self *Consumer) Listen(messages chan dispatcher_message.Message, t *tomb.Tomb) {
 	log.Print("Consumer: listening...")
 	self.transport = consumer_transport.NewConsumerTransport()
-	self.transport.Listen(messageChannel, t)
+	self.transport.Listen(messages, t)
 	if self.transport.GetError() != nil {
 		log.Fatal("Consumer: error during consumer initialization: %s", self.transport.GetError())
 	}
